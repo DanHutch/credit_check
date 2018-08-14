bfc66a8d6e942cdab40cbf2f2dfe86f8b39297c3
DanHutch <danhutch5280@gmail.com>
2018-08-14 12:38:11 -0600

    Adds credit_check iteration 1

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -2,6 +2,44 @@ card_number = "5541808923795240"
 
 # Your Luhn Algorithm Here
 
+card_integers =  []
+
+card_number.chars.each do |x|
+  card_integers << x.to_i
+end
+
+doubles = []
+
+card_integers.each_with_index do |integer, index|
+
+  if index == 0
+    doubles << integer * 2
+  elsif index % 2 == 0
+    doubles << integer * 2
+  else
+    doubles << integer
+  end
+
+end
+
+new_numbers = []
+
+doubles.each do |double|
+  if double > 9
+    new_numbers << double - 9
+  else
+    new_numbers << double
+  end
+end
+
+final_sum = new_numbers.sum
+
 # Output
 ## If it is valid, print "The number [card number] is valid!"
 ## If it is invalid, print "The number [card number] is invalid!"
+
+if final_sum % 10 == 0
+  puts "This card number is VALID."
+else
+  puts "This card number is INVALID"
+end
